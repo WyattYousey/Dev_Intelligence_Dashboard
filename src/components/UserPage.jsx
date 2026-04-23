485d9eb40b49d60e08b4512ea12db0e87ff5ea95
wyattYousey <quiz.nv1@gmail.com>
2026-04-23 17:08:31 -0500

    Implemented new profile markdown display using react-markdown and some emoji parsing libs, also added some subtle UI updates.

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -1,7 +1,25 @@
-import Header from './Header';
+import { useEffect, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 import './styles/UserPage.css';
+import Header from './Header';
+import { getUserReadMe } from '../utils/GithubApi';
+import { decodeBase64 } from '../utils/decodeBase64';
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
 
 const UserPage = ({ loading, setLoading, error, setError, user, setUser }) => {
+  const [readme, setReadMe] = useState('');
+
+  useEffect(() => {
+    async function fetchReadme() {
+      const content = await getUserReadMe(user.login);
+      if (content) {
+        setReadMe(decodeBase64(content));
+      }
+    }
+
+    fetchReadme();
+  }, [user]);
   return (
     <div className="user_page">
       <Header>
@@ -12,16 +30,30 @@ const UserPage = ({ loading, setLoading, error, setError, user, setUser }) => {
         />
         <div className="header__user-info">
           <h2>
-            {user.name || user.login} [@{user.login}]
+            {user.name || user.login} [
+            <span className="header__user-login">@{user.login}</span>]
           </h2>
           <p>{user.bio}</p>
-          <div className="user-header__stats">
+          <div className="header__user-stats">
             <span>{user.followers} followers</span>
             <span>{user.following} following</span>
             <span>{user.public_repos} repos</span>
           </div>
         </div>
       </Header>
+
+      <div className="user_page__main_content">
+        {readme && (
+          <div className="user_page__markdown">
+            <ReactMarkdown
+              remarkPlugins={[remarkGfm]}
+              rehypePlugins={[rehypeRaw]}
+            >
+              {readme}
+            </ReactMarkdown>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- src/utils/GithubApi.js
+++ src/utils/GithubApi.js
@@ -16,6 +16,24 @@ export async function getUser(username) {
   }
 }
 
+export async function getUserReadMe(username) {
+  try {
+    const result = await fetch(
+      `${baseUrl}/repos/${username}/${username}/readme`
+    );
+
+    if (!result.ok) {
+      throw new Error(`HTTP error! status: ${result.status}`);
+    }
+
+    const data = await result.json();
+    return data.content;
+  } catch (error) {
+    console.error('Fetch operation failed:', error.message);
+    return null;
+  }
+}
+
 export async function getRepos(username) {
   try {
     const result = await fetch(`${baseUrl}/users/${username}/repos`);

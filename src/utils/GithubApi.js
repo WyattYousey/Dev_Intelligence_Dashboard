d10200b61f7755c72efa0ff95f2372358017a792
wyattYousey <quiz.nv1@gmail.com>
2026-04-24 15:01:14 -0500

    Implement getRepo function for fetching individual repos

--- src/utils/GithubApi.js
+++ src/utils/GithubApi.js
@@ -3,12 +3,13 @@ const baseUrl = 'https://api.github.com';
 export async function getUser(username) {
   try {
     const result = await fetch(`${baseUrl}/users/${username}`);
-
+    
     if (!result.ok) {
       throw new Error(`HTTP error! status: ${result.status}`);
     }
-
+    
     const data = await result.json();
+    console.log('GET USER HIT', data);
     return data;
   } catch (error) {
     console.error('Fetch operation failed:', error.message);
@@ -37,6 +38,22 @@ export async function getUserReadMe(username) {
 export async function getRepos(username) {
   try {
     const result = await fetch(`${baseUrl}/users/${username}/repos`);
+
+    if (!result.ok) {
+      throw new Error(`HTTP error! status: ${result.status}`);
+    }
+
+    const data = await result.json();
+    return data;
+  } catch (error) {
+    console.error('Fetch operation failed:', error.message);
+    return null;
+  }
+}
+
+export async function getRepo(username, repo) {
+  try {
+    const result = await fetch(`${baseUrl}/repos/${username}/${repo}`);
 
     if (!result.ok) {
       throw new Error(`HTTP error! status: ${result.status}`);
